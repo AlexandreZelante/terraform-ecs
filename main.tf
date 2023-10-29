16690f8041c1ac04c0de17f6bb21fd9404788122
Alexandre Zelante <alexandre.zelante@outlook.com>
2023-10-29 19:31:05 -0300

    terraform main file

--- main.tf
+++ main.tf
@@ -1,3 +1,89 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+###############
+# VPC
+###############
+
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "sn1" {
+  cidr_block              = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "sn2" {
+  cidr_block              = "10.0.2.0/24"
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "sn3" {
+  cidr_block              = "10.0.3.0/24"
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "sg" {
+  name   = "sg"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description = "https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+###############
+# Repository
+###############
+
+resource "aws_ecr_repository" "repo" {
+  name                 = "app_repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.repo.repository_url
+}
+
+###############
+# ECS
+###############
+
+
 resource "aws_ecs_cluster" "ecs" {
   name = "app_cluster"
 }
